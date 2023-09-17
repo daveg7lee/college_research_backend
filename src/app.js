d5d43e4748b2b4349f5241875871f1e9d4e12b7f
daveg7lee <daveg7lee@gmail.com>
2023-09-17 09:51:29 +0900

    ✨ severless

---
+++ src/app.js
@@ -0,0 +1,55 @@
+async function routes(fastify, options) {
+  fastify.get("/", async function handler(request, reply) {
+    if (!request.params.id) {
+      reply.send("Id is required");
+      return;
+    }
+
+    const res = await axios.get(
+      `https://nces.ed.gov/collegenavigator/?id=${request.params.id}`
+    );
+
+    const html = cheerio.load(res.data);
+    const totalFee = html(
+      `#divctl00_cphCollegeNavBody_ucInstitutionMain_ctl00 > .tabconstraint > table.tabular > tbody > tr.level1indent:nth-child(20) > td:nth-child(5)`
+    ).text();
+    const accpetanceRate = html(
+      `#divctl00_cphCollegeNavBody_ucInstitutionMain_ctl04 > .tabconstraint > table.tabular > tbody > tr:nth-child(2) > td:nth-child(2)`
+    ).text();
+    const SATRequired = html(
+      `#divctl00_cphCollegeNavBody_ucInstitutionMain_ctl04 > .tabconstraint > table.tabular:nth-child(5) > tbody > tr:nth-child(1) > td:nth-child(2)`
+    ).text();
+    const SATOptional = html(
+      `#divctl00_cphCollegeNavBody_ucInstitutionMain_ctl04 > .tabconstraint > table.tabular:nth-child(5) > tbody > tr:nth-child(1) > td:nth-child(3)`
+    ).text();
+    const GPARequired = html(
+      `#divctl00_cphCollegeNavBody_ucInstitutionMain_ctl04 > .tabconstraint > table.tabular:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(2)`
+    ).text();
+    const GPAOptional = html(
+      `#divctl00_cphCollegeNavBody_ucInstitutionMain_ctl04 > .tabconstraint > table.tabular:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(3)`
+    ).text();
+
+    const SAT =
+      SATRequired === "X"
+        ? "Required"
+        : SATOptional === "X"
+        ? "Optional (not required but considered)"
+        : "Blind (not considered)";
+
+    const GPA =
+      GPARequired === "X"
+        ? "Required"
+        : GPAOptional === "X"
+        ? "Optional (not required but considered)"
+        : "Blind (not considered)";
+
+    reply.send({
+      total_fee: totalFee,
+      accpetance_rate: `${accpetanceRate.split("%")[0]}%`,
+      sat_required: SAT,
+      gpa_required: GPA,
+    });
+  });
+}
+
+export default routes;
